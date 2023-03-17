53adae31de42ba4b8d69b93b5481b1c66980ad01
raosk <oskari.rantamaula@gmail.com>
2023-03-17 19:41:17 +0200

    part 1

---
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.todo.ui
+
+import android.os.Bundle
+import androidx.compose.material.*
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.todo.ui.theme.TodoTheme
+import com.example.todo.viewmodel.TodoViewModel
+import androidx.compose.foundation.lazy.items
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TodoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TodoScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoScreen(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<String>) {
+    LazyColumn (
+        modifier = Modifier.padding(8.dp)
+    ){
+        items(todos) { todo->
+            Text(
+                text = todo,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TodoTheme {
+        TodoScreen()
+    }
+}
